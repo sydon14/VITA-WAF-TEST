14128d86f5ee0f04360c51c3ff62cc04b219516c
sydon14 <sydon_md@yahoo.com>
2025-04-13 07:18:23 -0400

    enable manual workflow dispatch for testing

--- VITA/VITA-WAF-terraform/agencies/dmv/main.tf
+++ VITA/VITA-WAF-terraform/agencies/dmv/main.tf
@@ -8,6 +8,3 @@ module "dmv_waf" {
   metric_name    = "dmv-waf-metrics"
   alb_arn        = "arn:aws:elasticloadbalancing:us-east-1:111122223333:loadbalancer/app/dmv-app/xyz"
 }
-
-# This is a simulation update for DMV WAF deployment
-# Manual CI test from Sydon
