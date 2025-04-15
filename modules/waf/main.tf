b081b1ed79ad80d3901211eee486bec770699672
Sydon <sydon_md@yahoo.com>
2025-04-15 14:40:31 -0400

    test with single waf rule to isolate creation issue

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -12,52 +12,25 @@ resource "aws_wafv2_web_acl" "web_acl" {
     sampled_requests_enabled   = true
   }
 
-  dynamic "rule" {
-    for_each = [
-      {
-        name        = "AWS-AWSManagedRulesSQLiRuleSet"
-        priority    = 1
-        metric_name = "SQLiRule"
-        rule_group  = "AWSManagedRulesSQLiRuleSet"
-      },
-      {
-        name        = "AWS-AWSManagedRulesXSSRuleSet"
-        priority    = 2
-        metric_name = "XSSRule"
-        rule_group  = "AWSManagedRulesXSSRuleSet"
-      },
-      {
-        name        = "AWS-AWSManagedRulesCommonRuleSet"
-        priority    = 3
-        metric_name = "CommonRule"
-        rule_group  = "AWSManagedRulesCommonRuleSet"
-      },
-      {
-        name        = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
-        priority    = 4
-        metric_name = "BadInputsRule"
-        rule_group  = "AWSManagedRulesKnownBadInputsRuleSet"
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
       }
-    ]
+    }
 
-    content {
-      name     = rule.value.name
-      priority = rule.value.priority
-
-      override_action {}
-
-      statement {
-        managed_rule_group_statement {
-          name        = rule.value.rule_group
-          vendor_name = "AWS"
-        }
-      }
-
-      visibility_config {
-        cloudwatch_metrics_enabled = true
-        metric_name                = rule.value.metric_name
-        sampled_requests_enabled   = true
-      }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRule"
+      sampled_requests_enabled   = true
     }
   }
 }
