c59de694b402fdb8376e217f1d63e4b892928906
Sydon <sydon_md@yahoo.com>
2025-04-17 13:28:28 -0400

    testing trigger for dot

--- agencies/dot/main.tf
+++ agencies/dot/main.tf
@@ -11,4 +11,4 @@ module "dot_waf" {
   blocked_ips         = var.blocked_ips
   geo_match_countries = var.geo_match_countries
 }
-# Triggering workflow test for DOT agency
+# Triggering workflow test for DOT agency via dev branch
